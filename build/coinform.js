f0312b1ace772f50034b82791e395816acddfa90
Omair Iqbal <omair.iqbal@scytl.com>
2019-11-21 17:09:10 +0100

    build and node_modules folders deleted

--- build/coinform.js
+++ build/coinform.js
@@ -15361,27 +15361,6 @@ const thresholdLevels = {
   low: 80
 };
 
-var opts = {
-  lines: 20, // The number of lines to draw
-  length: 40, // The length of each line
-  width: 20, // The line thickness
-  radius: 35, // The radius of the inner circle
-  scale: 1, // Scales overall size of the spinner
-  corners: 1, // Corner roundness (0..1)
-  color: '#ff0000', // CSS color or array of colors
-  fadeColor: 'transparent', // CSS color or array of colors
-  speed: 2.2, // Rounds per second
-  rotate: 90, // The rotation offset
-  animation: 'spinner-line-fade-more', // The CSS animation name for the lines
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  zIndex: 2e9, // The z-index (defaults to 2000000000)
-  className: 'spinner', // The CSS class to assign to the spinner
-  top: '50%', // Top position relative to parent
-  left: '50%', // Left position relative to parent
-  shadow: '0 0 1px transparent', // Box-shadow for the lines
-  position: 'absolute' // Element positioning
-};
-
 const usersCache = {};
 
 /*
@@ -15493,7 +15472,7 @@ const newTweetCallback = (tweetInfo) => {
           console.log(firstRes);
         }
       }).catch(err => console.log(err));     
-        
+
     }
   
     classifyTweet(tweetInfo, score);
