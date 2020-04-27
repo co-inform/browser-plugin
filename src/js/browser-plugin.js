900df22fe8cdf406c706aecae74f266856c0c630
Aleix <alxer.miler@gmail.com>
2020-04-27 12:16:26 +0200

    Merge conflict solved

--- src/js/browser-plugin.js
+++ src/js/browser-plugin.js
@@ -252,16 +252,12 @@ const newTweetCallback = (tweetInfo) => {
     pluginCache[tweetInfo.id] = false;
   }
 
-<<<<<<< HEAD
-=======
   if (!tweetInfo.domObject.toolBar) {
-
     let toolbar = createToolbar(tweetInfo);
     tweetInfo.domObject.prepend(toolbar);
     tweetInfo.domObject.toolBar = true;
   }
 
->>>>>>> b47b97a0e147405bdeb00abc260368c17a2b1ff4
   // If the tweet has already been tagged then we directly classify it
   if (pluginCache[tweetInfo.id]) {
     logger.logMessage(CoInformLogger.logTypes.debug, `Already analyzed tweet`, tweetInfo.id);
@@ -292,12 +288,6 @@ const newTweetCallback = (tweetInfo) => {
     // console.error(err);
 
   });
-  
-  if (!tweetInfo.domObject.toolBar) {
-    let toolbar = createToolbar(tweetInfo, acurracyLabel);
-    tweetInfo.domObject.prepend(toolbar);
-    tweetInfo.domObject.toolBar = true;
-  }
 
 };
 
